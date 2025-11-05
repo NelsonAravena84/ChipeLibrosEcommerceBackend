0eebaa7d3f2b1c33188989a88622d72a95ac6aac
nelson <nelsonaravena2024@gmail.com>
2025-11-05 16:47:07 -0300

    .

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,28 @@
 import express from 'express';
 import router from './routes/productosRoutes.js';
 import dotenv from 'dotenv';
-import cors from 'cors';
+import cors from 'cors'
 dotenv.config();
+
 const app = express();
+
 // Permitir CORS para tu frontend
 app.use(cors({
-    origin: 'http://localhost:3000', // origen permitido
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
+  origin: [
+    'http://localhost:3000',
+    'https://ecommercechipelibros.pages.dev/'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
 }));
-app.use(cors({
-    origin: 'https://f8e2b65d.ecommercechipelibros.pages.dev/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}));
+
+
 app.use(express.json());
+
 // Rutas
 app.use('/api/productos', router);
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-//# sourceMappingURL=index.js.map
